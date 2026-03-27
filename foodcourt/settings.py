381996c1857bd994e98f536a20a208302c003296
akshat5522 <akshatsharma6218@gmail.com>
2026-03-27 18:37:34 +0530

    Fix render secret key

--- foodcourt/settings.py
+++ foodcourt/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-temp-key-for-render'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = "True"
 
 
 ALLOWED_HOSTS = ['*']

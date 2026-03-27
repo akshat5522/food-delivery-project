8f0f300ee422e0089d787fe5529e78de9c0798d5
akshat5522 <akshatsharma6218@gmail.com>
2026-03-27 16:21:55 +0530

    Food Delivery Project Clean Structure

---
+++ foods/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name='home'),
+    path("home/", views.home, name='home'),
+    path("login_page/", views.login_page, name='login'),
+    path("register_page/", views.register_page, name='register'),
+    path('add-cart/<pizza_uid>', views.add_cart, name='add_cart'),
+    path("cart/", views.cart, name='cart'),
+    path("remove_cart_items/<cart_item_uid>", views.remove_cart_items, name='remove_cart_items'),
+    path('orders/', views.orders, name='orders'),
+    path('success/', views.success, name='success')
+]
